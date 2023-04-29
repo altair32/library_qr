7c504fdc2813719d69d25db8bdecbeace9013d37
brisingr24 <rawatadity234@gmail.com>
2023-04-29 23:24:44 +0530

    Scratch --Aditya

---
+++ lib/screens/register.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Center(child: Text("Register"),),
+      ),
+      body: Column(
+        children: [
+          Container()
+        ],
+      ),
+    );
+  }
+}
