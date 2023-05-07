2359cac726816ac749f5a97d64cfbfdcfb21582d
brisingr24 <rawatadity234@gmail.com>
2023-05-07 19:32:23 +0530

    google auth -- aditya

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,28 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:library_qr/screens/register.dart';
+import 'package:library_qr/widgets/authentication.dart';
+import 'package:provider/provider.dart';
+
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) =>GoogleSignInProvider())
+        // Provider<FirebaseAuthMethods>(
+        //   create: (_) => FirebaseAuthMethods(),
+        // ),
+        // StreamProvider(
+        //   create: (context) => context.read<FirebaseAuthMethods>().authState,
+        //   initialData: null,
+        // ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +32,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Register(),
     );
   }
