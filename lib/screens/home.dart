12fc0aa07c90704643c32eb417da465e4f8f8b82
brisingr24 <rawatadity234@gmail.com>
2023-05-18 18:57:03 +0530

    AUTH HAS BEEN FINISHED

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:library_qr/screens/register.dart';
-
 import 'loggedIn.dart';
 
 class Home extends StatefulWidget {
